c2adcb75d1ceae934a8fba4a1655b84e7e3b3018
Angelica <Angelica137@users.noreply.github.com>
2022-02-27 18:05:17 +0000

    add count_failed_students(), green

--- scripts/loops.py
+++ scripts/loops.py
@@ -6,3 +6,15 @@ def round_scores(student_scores: list) -> list:
     for i in range(len(rounded_scores)):
         rounded_scores[i] = round(rounded_scores[i])
     return rounded_scores
+
+
+def count_failed_students(student_scores: list) -> list:
+    """
+    Takes a list of student scores and 
+    returns the cound to failed students
+    """
+    count = 0
+    for score in student_scores:
+        if score <= 40:
+            count += 1
+    return count
