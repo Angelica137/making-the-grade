ddc96d02fe5bc77e30f803f9be8d7e685da7e263
Angelica <Angelica137@users.noreply.github.com>
2022-02-27 17:57:36 +0000

    test count_failed_students(), red

--- tests/test_loops.py
+++ tests/test_loops.py
@@ -6,3 +6,8 @@ def test_round_scores():
                       30.55, 25.45, 80.45, 95.3, 38.7, 40.3]
     assert round_scores(student_scores) == [
         90, 40, 55, 70, 31, 25, 80, 95, 39, 40]
+
+
+def test_count_failed_students():
+    student_scores = [90, 40, 55, 70, 30, 25, 80, 95, 38, 40]
+    assert count_failed_students(student_scores) == 5
