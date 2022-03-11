d0279b0cc4b44dc847a725b4b172963a5da72e3d
Angelica <Angelica137@users.noreply.github.com>
2022-03-11 18:30:52 +0000

    refactor round_scores() with list comprehension

--- scripts/loops.py
+++ scripts/loops.py
@@ -4,11 +4,13 @@ import math
 def round_scores(student_scores: list) -> list:
     """
     Returns a list of student scores rounded to nearest integer
-    """
+
     rounded_scores = student_scores
     for i in range(len(rounded_scores)):
         rounded_scores[i] = round(rounded_scores[i])
     return rounded_scores
+    """
+    return [round(l) for l in student_scores]
 
 
 def count_failed_students(student_scores: list) -> list:
